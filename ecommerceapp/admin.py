37479e3c1d191d225f04350ad7a25afcef0d2426
Himanshu Pathak <himanshupathak@Himanshus-MacBook-Air.local>
2024-03-05 14:25:51 +0530

    added vercel conf

--- ecommerceapp/admin.py
+++ ecommerceapp/admin.py
@@ -4,3 +4,5 @@ from ecommerceapp.models import *
 # Register your models here.
 admin.site.register(Contact)
 admin.site.register(Product)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
